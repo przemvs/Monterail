9c8caf9ded7e1c597acb152495cae1225f2e2219
Przemysław Jóźwik <przemyslaw.jozwik96@gmail.com>
2018-07-25 16:45:18 +0200

    Change non state components

--- src/views/CreateEvent/CreateEventForm/index.js
+++ src/views/CreateEvent/CreateEventForm/index.js
@@ -1,4 +1,4 @@
-import React, {Component, Fragment} from 'react'
+import React, {Fragment} from 'react'
 import PropTypes from 'prop-types'
 import About from './About'
 import Coordinator from './Coordinator'
@@ -8,24 +8,20 @@ import {validate} from '../validators'
 import {reduxForm} from 'redux-form'
 import EventAdded from './EventAdded'
 
-class CreateEventForm extends Component {
-  render () {
-    const {employees, categories, handleSubmit, submitSucceeded} = this.props
+const CreateEventForm = ({employees, categories, handleSubmit, submitSucceeded}) => {
+  return (
+    <Fragment>
+      {submitSucceeded ? <EventAdded /> : (
+        <form>
+          <About categories={categories} />
+          <Coordinator employees={employees} />
+          <Date />
 
-    return (
-      <Fragment>
-        {submitSucceeded ? <EventAdded /> : (
-          <form>
-            <About categories={categories} />
-            <Coordinator employees={employees} />
-            <Date />
-
-            <Button onClick={handleSubmit}>Publish event</Button>
-          </form>
-        )}
-      </Fragment>
-    )
-  }
+          <Button onClick={handleSubmit}>Publish event</Button>
+        </form>
+      )}
+    </Fragment>
+  )
 }
 
 CreateEventForm.propTypes = {
